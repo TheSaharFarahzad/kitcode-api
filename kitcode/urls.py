7546c77f50019aa2e6b9a7eff8f1acdd369810c5
Sahar <s.farahzad91@gmail.com>
2024-10-07 17:16:49 +0330

    feat: create a basic Django REST Framework API

--- kitcode/urls.py
+++ kitcode/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', HelloWorldView.as_view(), name='hello_world'),
 ]
