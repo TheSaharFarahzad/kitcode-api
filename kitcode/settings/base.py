b1d44b0b1b2a7d2548dcd37426f6e8138dc37024
Sahar <s.farahzad91@gmail.com>
2024-11-28 22:28:41 +0330

    feat: configure email settings with environment variables

--- kitcode/settings/base.py
+++ kitcode/settings/base.py
@@ -96,7 +96,18 @@ REST_AUTH = {
     "SESSION_LOGIN": False,
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = env(
+    "EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+)
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="webmaster@localhost")
+DEFAULT_TO_EMAIL = env("DEFAULT_TO_EMAIL", default=EMAIL_HOST_USER)
+
 
 ROOT_URLCONF = "kitcode.urls"
 
